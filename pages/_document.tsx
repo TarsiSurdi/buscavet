63c2e33af02faac1a7f750e4bab8f714528af6a5
Tarcísio Surdi <tarsisurdi@gmail.com>
2022-06-09 18:56:27 -0300

    Added basic support for NextUI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,50 @@
-import { Html, Main, NextScript, Head } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { CssBaseline } from "@nextui-org/react";
+import React from "react";
 
-const Document = () => {
-  return (
-    <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fugaz+One&display=swap"
-          rel="stylesheet"
-        />
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles]),
+    };
+  }
 
-        <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
-        <link rel="icon" href="/favicons/favicon.ico" />
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fugaz+One&display=swap"
+            rel="stylesheet"
+          />
 
-        <link rel="manifest" href="/manifest.webmanifest" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+          <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" href="/favicons/favicon.ico" />
 
-export default Document;
+          <link rel="manifest" href="/manifest.webmanifest" />
+          {/* {CssBaseline.flush()} */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/medications.tsx
+++ pages/medications.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@nextui-org/react";
 import { RiMedicineBottleFill } from "react-icons/ri";
+
 import Layout from "../components/Layout";
 import PageNavigation from "../components/PageNavigation";
 
@@ -9,6 +11,7 @@ const Medications = () => {
       url="https://buscavet.vercel.app/medications"
     >
       <PageNavigation icon={<RiMedicineBottleFill />} title="Medicações" />
+      <Button>Teste NextUI</Button>
     </Layout>
   );
 };
