90724adc9423dd3a779e2c641f2fba4e0c4df944
Tarcísio Surdi <tarsisurdi@gmail.com>
2022-06-08 22:11:46 -0300

    Added lang attribute to <html>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
-
 const withPWA = require("next-pwa");
 
 const nextConfig = withPWA({
   pwa: {
     dest: "public",
+  },
+  i18n: {
+    locales: ["pt-br"],
+    defaultLocale: "pt-br",
   },
 });
 
