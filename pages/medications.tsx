297f138e63f353e9b3799e903e7619dfbe3e0030
Tarcísio Surdi <tarsisurdi@gmail.com>
2022-06-10 22:57:19 -0300

    Added basic table for medications page

--- pages/medications.tsx
+++ pages/medications.tsx
@@ -15,37 +15,71 @@ const Medications = () => {
         <Table
           aria-label="Example table with static content"
           css={{
-            height: "auto",
+            height: "calc($space$14 * 8)",
             minWidth: "100%",
           }}
         >
           <Table.Header>
             <Table.Column>NAME</Table.Column>
             <Table.Column>ROLE</Table.Column>
-            <Table.Column>STATUS</Table.Column>
           </Table.Header>
           <Table.Body>
             <Table.Row key="1">
               <Table.Cell>Tony Reichert</Table.Cell>
               <Table.Cell>CEO</Table.Cell>
-              <Table.Cell>Active</Table.Cell>
             </Table.Row>
             <Table.Row key="2">
               <Table.Cell>Zoey Lang</Table.Cell>
               <Table.Cell>Technical Lead</Table.Cell>
-              <Table.Cell>Paused</Table.Cell>
             </Table.Row>
             <Table.Row key="3">
               <Table.Cell>Jane Fisher</Table.Cell>
               <Table.Cell>Senior Developer</Table.Cell>
-              <Table.Cell>Active</Table.Cell>
             </Table.Row>
             <Table.Row key="4">
               <Table.Cell>William Howard</Table.Cell>
               <Table.Cell>Community Manager</Table.Cell>
-              <Table.Cell>Vacation</Table.Cell>
+            </Table.Row>
+            <Table.Row key="5">
+              <Table.Cell>Tarcísio Surdi</Table.Cell>
+              <Table.Cell>Next.js Developer</Table.Cell>
+            </Table.Row>
+            <Table.Row key="6">
+              <Table.Cell>William Howard</Table.Cell>
+              <Table.Cell>Community Manager</Table.Cell>
+            </Table.Row>
+            <Table.Row key="7">
+              <Table.Cell>William Howard</Table.Cell>
+              <Table.Cell>Community Manager</Table.Cell>
+            </Table.Row>
+            <Table.Row key="8">
+              <Table.Cell>William Howard</Table.Cell>
+              <Table.Cell>Community Manager</Table.Cell>
+            </Table.Row>
+            <Table.Row key="9">
+              <Table.Cell>William Howard</Table.Cell>
+              <Table.Cell>Community Manager</Table.Cell>
+            </Table.Row>
+            <Table.Row key="10">
+              <Table.Cell>William Howard</Table.Cell>
+              <Table.Cell>Community Manager</Table.Cell>
+            </Table.Row>
+            <Table.Row key="11">
+              <Table.Cell>William Howard</Table.Cell>
+              <Table.Cell>Community Manager</Table.Cell>
+            </Table.Row>
+            <Table.Row key="12">
+              <Table.Cell>William Howard</Table.Cell>
+              <Table.Cell>Community Manager</Table.Cell>
             </Table.Row>
           </Table.Body>
+          <Table.Pagination
+            shadow
+            noMargin
+            align="center"
+            rowsPerPage={5}
+            onPageChange={(page) => console.log({ page })}
+          />
         </Table>
       </Container>
     </Layout>
