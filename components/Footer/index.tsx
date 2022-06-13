99e8d12530d9765186603eb60dc8b010afe16c9a
Tarcísio Surdi <tarsisurdi@gmail.com>
2022-06-13 07:38:26 -0300

    Added phrase changing to footer component

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,12 +1,56 @@
-import { Container, Text } from "@nextui-org/react";
+import { Text } from "@nextui-org/react";
 
 import styles from "./Footer.module.scss";
+import { useEffect, useState } from "react";
 
-const Footer = () => {
+function getRandomIntInclusive(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+export interface FooterProps {
+  phrases?: string[];
+}
+
+const phraseList = [
+  "🍪 Esse site não utiliza cookies",
+  "👨‍💻 Desenvolvido por Tarcísio Surdi",
+  "👩‍⚕️ Revisão por Julia Ellen",
+  "🎓 Um trabalho proveniente da UNOESC",
+];
+
+const Footer: React.FC<FooterProps> = ({ phrases = phraseList }) => {
+  const [phrasesArray, setPhrasesArray] = useState(phrases);
+  const [phrase, setPhrase] = useState("🍪 Esse site não utiliza cookies");
+
+  useEffect(() => {
+    const intervalId = setTimeout(() => {
+      console.log(phrasesArray);
+
+      if (phrasesArray.length === 0) {
+        setPhrasesArray(phrases);
+        return;
+      } else {
+        const updatedPhrase =
+          phrasesArray[getRandomIntInclusive(0, phrasesArray.length - 1)];
+
+        setPhrase(updatedPhrase);
+        setPhrasesArray((prevState) => {
+          prevState = prevState.filter((item) => item !== updatedPhrase);
+          return prevState;
+        });
+      }
+    }, 5 * 1000);
+
+    return () => {
+      clearTimeout(intervalId);
+    };
+  }, [phrasesArray]);
+
   return (
     <footer className={styles.footer}>
       <Text h5 css={{ ta: "center" }}>
-        🍪 Esse site não utiliza cookies
+        {phrase}
       </Text>
     </footer>
   );
