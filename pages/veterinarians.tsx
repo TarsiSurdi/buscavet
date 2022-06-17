3269fab14150a967541fcd2d362d6c101cf8fabf
Tarcísio Surdi <tarsisurdi@gmail.com>
2022-06-17 19:28:57 -0300

    Added /veterianarians content

--- pages/veterinarians.tsx
+++ pages/veterinarians.tsx
@@ -1,7 +1,8 @@
-import { Collapse, Container, Text } from "@nextui-org/react";
+import { Avatar, Collapse, Container, Grid, Text } from "@nextui-org/react";
 import { FaHandHoldingMedical } from "react-icons/fa";
 import Layout from "../components/Layout";
 import PageNavigation from "../components/PageNavigation";
+import StyledBadge from "../components/Badge";
 
 const Veterinarians = () => {
   return (
@@ -15,12 +16,74 @@ const Veterinarians = () => {
           title="Veterinários Habilitados"
         />
         <Collapse.Group bordered>
-          <Collapse title="Julia Ellen">
+          <Collapse
+            title="Igor Augusto Louvatel"
+            contentLeft={<Avatar squared src="./avatars/igor_louvatel.jpg" />}
+          >
             <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              Graduado em Medicina Veterinária pela Universidade do Oeste de
+              Santa Catarina.
+            </Text>
+            <Text>
+              Habilitações para exame:
+              <StyledBadge type="brucelose">Brucelose</StyledBadge>
+              <StyledBadge type="tuberculose">tuberculose</StyledBadge>
+            </Text>
+          </Collapse>
+          <Collapse
+            title="Julia Ellen"
+            contentLeft={<Avatar squared src="./avatars/julia_ellen.jpg" />}
+          >
+            <Text>
+              Graduada em Medicina Veterinária pela Universidade do Oeste de
+              Santa Catarina.
+            </Text>
+            <Text>
+              Habilitações para exame:
+              <StyledBadge type="brucelose">Brucelose</StyledBadge>
+              <StyledBadge type="tuberculose">tuberculose</StyledBadge>
+            </Text>
+          </Collapse>
+          <Collapse
+            title="Rodrigo Zanini"
+            contentLeft={<Avatar squared src="./avatars/rodrigo_zanini.jpg" />}
+          >
+            <Text>
+              Graduado em Medicina Veterinária pela Universidade do Oeste de
+              Santa Catarina.
+            </Text>
+            <Text>
+              Habilitações para exame:
+              <StyledBadge type="brucelose">Brucelose</StyledBadge>
+              <StyledBadge type="tuberculose">tuberculose</StyledBadge>
+            </Text>
+          </Collapse>
+          <Collapse
+            title="Thiago Miranda"
+            contentLeft={<Avatar squared src="./avatars/thiago_miranda.jpg" />}
+          >
+            <Text>
+              Graduado em Medicina Veterinária pela Universidade do Oeste de
+              Santa Catarina.
+            </Text>
+            <Text>
+              Habilitações para exame:
+              <StyledBadge type="brucelose">Brucelose</StyledBadge>
+              <StyledBadge type="tuberculose">tuberculose</StyledBadge>
+            </Text>
+          </Collapse>
+          <Collapse
+            title="Eloisa Lopes"
+            contentLeft={<Avatar squared src="./avatars/eloisa_lopes.jpg" />}
+          >
+            <Text>
+              Graduada em Medicina Veterinária pela Universidade do Oeste de
+              Santa Catarina.
+            </Text>
+            <Text>
+              Habilitações para exame:
+              <StyledBadge type="brucelose">Brucelose</StyledBadge>
+              <StyledBadge type="tuberculose">tuberculose</StyledBadge>
             </Text>
           </Collapse>
         </Collapse.Group>
